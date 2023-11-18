06dd6a8b5912232e4b24fd5ab125a9880c6e23ea
LatinResearchProgram <151351181+LatinResearchProgram@users.noreply.github.com>
2023-11-18 08:45:19 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# lxcv.explorecsr
---
+++ js/custom.js
@@ -0,0 +1,29 @@
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+
